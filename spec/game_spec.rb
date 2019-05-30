86b5229befb8d53cf88fe820a67c9522ef16408a
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-30 13:48:13 +0100

    added test and code for attack in Game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,9 @@
 require 'game'
 
-describe Game do 
-
+describe Game do
+  let(:player) { spy(:player) } 
+  it 'can call take_damage' do 
+    subject.attack(player)
+    expect(player).to have_received(:take_damage)
+  end 
 end
