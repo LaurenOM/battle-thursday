1a06c073c327f955662902ae5afe591c51636ec0
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-30 14:16:05 +0100

    decoupled test dependency of player from game

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,10 @@
+require 'player'
 class Game
   def attack(player)
     player.take_damage
+  end
+
+  def add_player(name, player = Player)
+    player.new(name)
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,17 @@
 require 'game'
 
 describe Game do
+  let(:player_class) { spy(:player_class, :new => player) }
   let(:player) { spy(:player) } 
+  
   it 'can call take_damage' do 
     subject.attack(player)
     expect(player).to have_received(:take_damage)
-  end 
+  end
+
+  it 'can create a player with a name' do 
+    game = Game.new
+    game.add_player("Player1", player_class)
+    expect(player_class).to have_received(:new).with("Player1")
+  end
 end
